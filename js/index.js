724fab11b59d0b0adbea4bc5aa0f6a0ceb796c94
gonzaloperrone <gonzalodamianperrone@gmail.com>
2021-12-15 11:17:51 -0300

    change

---
+++ js/index.js
@@ -0,0 +1,63 @@
+// Botón "cerrar" y añadirlo a cada elemento de la lista
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Eliminar todas las tareas de la lista
+function deleteAll(){
+var element  = document.getElementById("myUL");
+while (element.firstChild) {
+  element.removeChild(element.firstChild);
+}
+}
+
+// Botón de cierre para ocultar el elemento de la lista actual
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// Símbolo "marcado" al hacer clic en un elemento de la lista
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Nuevo elemento de lista al hacer clic en el botón "Agregar"
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("Tienes que escribir algo!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
